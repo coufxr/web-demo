210db5057cef507251d1ed9597411381da8dd392
coufxr <mu993364@163.com>
2024-02-05 15:05:42 +0800

    feat: logging/orm

--- src/app/user/view.rs
+++ src/app/user/view.rs
@@ -1,17 +1,35 @@
-use axum::{extract::Path, Json};
+use std::sync::Arc;
 
+use axum::{extract::Path, Extension, Json};
+use sea_orm::{ColumnTrait, EntityTrait, QueryFilter};
+
+use crate::states::AppState;
+
+use super::models::edu_account;
 use super::schemas::{UserInput, UserOutput};
 
-pub async fn user_list() -> Json<Vec<UserOutput>> {
-    let mut data: Vec<UserOutput> = Vec::new();
-    for i in 0..10 {
-        data.push(UserOutput {
-            id: i,
-            name: format!("name {i}"),
-            data: format!("data {i}"),
-        });
+// Extension 扩展引入需要与main中注册的元素一致
+pub async fn user_list(Extension(state): Extension<Arc<AppState>>) -> Json<Vec<UserOutput>> {
+    let qs = edu_account::Entity::find()
+        .filter(edu_account::Column::AccountType.eq(2))
+        .all(&state.conn)
+        .await;
+
+    match qs {
+        Ok(data) => {
+            // 将查询结果转换为Vec<User>
+            let mut result: Vec<UserOutput> = Vec::new();
+            for user in data {
+                result.push(UserOutput {
+                    id: user.id,
+                    name: user.name,
+                    data: user.gender.to_string(),
+                });
+            }
+            return Json(result);
+        }
+        Err(_) => todo!(),
     }
-    return Json(data);
 }
 
 pub async fn user_detail(Path(input): Path<UserInput>) -> Json<UserOutput> {

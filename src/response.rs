b61f97a8a75d22d03d2f7da1db0624b8ecac8908
wulang <wulang@szlhxx.com>
2024-02-06 10:23:23 +0800

    feat: 返回统一的结构体

---
+++ src/response.rs
@@ -0,0 +1,39 @@
+use axum::response::{IntoResponse, Response};
+use axum::Json;
+use serde::Serialize;
+
+#[derive(Debug, Serialize)]
+pub struct JsonResponse<T: Serialize> {
+    code: u16,
+    message: String,
+    data: Option<T>,
+}
+
+impl<T> JsonResponse<T>
+where
+    T: Serialize,
+{
+    pub fn new(code: u16, msg: String, data: Option<T>) -> Self {
+        Self {
+            code,
+            message: msg,
+            data,
+        }
+    }
+    pub fn success(data: T) -> Self {
+        Self::new(0, "OK".to_string(), Some(data))
+    }
+    pub fn error(code: u16, msg: String) -> Self {
+        Self::new(code, msg, None)
+    }
+}
+
+impl<T> IntoResponse for JsonResponse<T>
+where
+    T: Serialize,
+{
+    fn into_response(self) -> Response {
+        let res = Json(self);
+        res.into_response()
+    }
+}

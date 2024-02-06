e082b64bf7fcbe24363b1aa5f57e7b7192c60d9f
wulang <wulang@szlhxx.com>
2024-02-06 15:33:53 +0800

    feat: sea-orm query

--- src/app/user/schemas.rs
+++ src/app/user/schemas.rs
@@ -1,6 +1,23 @@
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize)]
+pub struct UserListInput {
+    pub name: Option<String>,
+    pub telephone: Option<String>,
+    pub page: Option<u64>,
+    pub page_size: Option<u64>,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct UserListOutput {
+    pub id: i32,
+    pub uid: String,
+    pub account_name: Option<String>,
+    pub name: String,
+    pub gender: i8,
+}
+
+#[derive(Serialize, Deserialize)]
 pub struct UserInput {
     pub id: i32,
 }

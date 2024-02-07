61a1eca7c7b229118ecef643eb6c32f13a25a5c7
coufxr <mu993364@163.com>
2024-02-07 16:07:38 +0800

    feat: anyhow 处理错误信息

---
+++ src/error.rs
@@ -0,0 +1,27 @@
+use axum::http::StatusCode;
+use axum::response::{IntoResponse, Response};
+
+// 定义一个自己的 `anyhow::Error`.
+// 实现 IntoResponse 的 AppError
+pub struct AppError(anyhow::Error);
+
+impl IntoResponse for AppError {
+    fn into_response(self) -> Response {
+        (
+            StatusCode::INTERNAL_SERVER_ERROR,
+            format!("Something went wrong: {}", self.0),
+        )
+            .into_response()
+    }
+}
+
+// This enables using `?` on functions that return `Result<_, anyhow::Error>` to turn them into
+// `Result<_, AppError>`. That way you don't need to do that manually.
+impl<E> From<E> for AppError
+where
+    E: Into<anyhow::Error>,
+{
+    fn from(err: E) -> Self {
+        Self(err.into())
+    }
+}

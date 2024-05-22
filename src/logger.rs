af565662a645ad01bd46f97d919cb5a7c5f6b7e3
coufxr <mu993364@163.com>
2024-05-22 18:02:33 +0800

    refactor: logger init

--- src/logger.rs
+++ src/logger.rs
@@ -1,10 +1,9 @@
 use std::str::FromStr;
 
 use chrono::Local;
-use tracing::Level;
-use tracing_appender::non_blocking::WorkerGuard;
-use tracing_subscriber::fmt::format::Writer;
-use tracing_subscriber::fmt::time::FormatTime;
+use tracing::{Level, subscriber};
+use tracing_appender::{non_blocking, non_blocking::WorkerGuard, rolling};
+use tracing_subscriber::fmt::{format::Writer, time::FormatTime};
 
 use crate::configs::Configs;
 
@@ -21,26 +20,30 @@ pub async fn init(cfg: &Configs) -> WorkerGuard {
     let (non_blocking, guard) = if cfg.server.env == "prod" {
         // 使用tracing_appender，指定日志的输出目标位置
         // 参考: https://docs.rs/tracing-appender/latest/tracing_appender/index.html
-        let (non_blocking, guard) = tracing_appender::non_blocking(
-            tracing_appender::rolling::daily(&cfg.log.path, &cfg.log.filename),
-        );
-        (non_blocking, guard)
+        let file_appender = rolling::daily(&cfg.log.path, &cfg.log.filename);
+        non_blocking(file_appender)
     } else {
-        let (non_blocking, guard) = tracing_appender::non_blocking(std::io::stdout());
-        (non_blocking, guard)
+        non_blocking(std::io::stdout())
     };
 
-    // 初始化并设置日志格式(定制和筛选日志)
-    tracing_subscriber::fmt()
-        .with_max_level(level) //只有Debug 模式下才能打印sea-orm的完整sql日志
-        .with_file(true)
-        .with_line_number(true) // 写入标准输出
-        .with_ansi(true) // 关掉ansi的颜色输出功能
-        .with_timer(LocalTimer)
+    // 设置日志格式(定制和筛选日志)
+    let subscriber = tracing_subscriber::fmt()
+        .with_max_level(level) // 最大日志级别
         .with_writer(non_blocking)
-        // .json()
-        // .flatten_event(true)
-        .init(); // 初始化并将SubScriber设置为全局SubScriber
+        .with_ansi(true) // 显示ansi的颜色输出
+        .with_timer(LocalTimer) // 日期格式
+        .with_file(false) // 显示源代码文件路径
+        .with_line_number(true) // 显示源代码行号
+        .with_thread_ids(true) // 显示记录事件的线程ID
+        .with_target(true) // 显示事件的目标（模块路径）
+        // .compact() // 使用更紧凑、缩写的日志格式
+        .pretty() // 漂亮的多行日志,用于本地开发和调试
+        // .json() // json格式
+        // .flatten_event(true) // 为json展平事件元数据
+        .finish(); // 建立订阅服务器
+
+    // 设置为全局SubScriber
+    subscriber::set_global_default(subscriber).unwrap();
 
     guard
 }
